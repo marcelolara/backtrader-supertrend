31f154feb0aa6fa172816589338775bb82cb42c5
Marcelo Lara <chelop@gmail.com>
2021-10-17 01:13:02 -0300

    Adding feed data

--- backtest.py
+++ backtest.py
@@ -1,7 +1,22 @@
+import datetime
+import os.path
+import sys
 import backtrader as bt
 
 cerebro = bt.Cerebro()
-cerebro.broker.setcash(100000)
+
+modpath = os.path.dirname(os.path.abspath(sys.argv[0]))
+datapath = os.path.join(modpath, 'datas/orcl-1995-2014.txt')
+
+data = bt.feeds.YahooFinanceData(
+    dataname=datapath,
+    fromdate=datetime.datetime(2000, 1, 1),
+    todate=datetime.datetime(2000, 12, 31),
+    reverse=False)
+
+cerebro.adddata(data)
+
+cerebro.broker.setcash(100000.0)
 
 print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
 
