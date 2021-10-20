1b1d64474fda22eabf7a3e4f1421e3989e72dd73
Marcelo Lara <chelop@gmail.com>
2021-10-20 19:34:00 -0300

    Adding commission

--- backtest.py
+++ backtest.py
@@ -10,7 +10,7 @@ import backtrader as bt
 class TestStrategy(bt.Strategy):
 
     def log(self, txt, dt=None):
-        ''' Logging function for this strategy '''
+        """ Logging function for this strategy """
         dt = dt or self.datas[0].datetime.date(0)
         print('%s, %s' % (dt.isoformat(), txt))
 
@@ -18,8 +18,10 @@ class TestStrategy(bt.Strategy):
         # Keep a reference to the "close" line in the data[0] dataseries
         self.dataclose = self.datas[0].close
 
-        # To keep track of pending orders
+        # To keep track of pending orders and buy price/commission
         self.order = None
+        self.buyprice = None
+        self.buycomm = None
 
     def notify_order(self, order):
         if order.status in [order.Submitted, order.Accepted]:
@@ -30,9 +32,20 @@ class TestStrategy(bt.Strategy):
         # Attention: broker could reject order if not enough cash
         if order.status in [order.Completed]:
             if order.isbuy():
-                self.log('BUY EXECUTED, %.2f' % order.executed.price)
-            elif order.issell():
-                self.log('SELL EXECUTED, %.2f' % order.executed.price)
+                self.log(
+                    'BUY EXECUTED, Price: %.2f, Cost: %.2f, Comm: %.2f' %
+                    (order.executed.price,
+                     order.executed.value,
+                     order.executed.comm))
+
+                self.buyprice = order.executed.price
+                self.buycomm = order.executed.comm
+            else:  # Sell
+                self.log(
+                    'SELL EXECUTED, Price: %.2f, Cost: %.2f, Comm: %.2f' %
+                    (order.executed.price,
+                     order.executed.value,
+                     order.executed.comm))
 
             self.bar_executed = len(self)
 
@@ -42,15 +55,22 @@ class TestStrategy(bt.Strategy):
         # Write down: no pending order
         self.order = None
 
+    def notify_trade(self, trade):
+        if not trade.isclosed:
+            return
+
+        self.log('OPERATION PROFIT, GROSS: %.2f, NET: %.2f' %
+                 (trade.pnl, trade.pnlcomm))
+
     def next(self):
         # Simply log the closing price of the series from the reference
         self.log('Close, %.2f' % self.dataclose[0])
 
-        # Check if an order is pending ... if yes, we cannot send a 2nd on
+        # Check if an order is pending ... if yes, we cannot send a 2nd one
         if self.order:
             return
 
-        # Check if we are inf the market
+        # Check if we are in the market
         if not self.position:
 
             # Not yet ... we MIGHT BUY if ...
@@ -103,6 +123,9 @@ cerebro.adddata(data)
 
 # Set oru desired cash start
 cerebro.broker.setcash(100000.0)
+
+# Set the commission - 0.1% ... divide by 100 to remove the %
+cerebro.broker.setcommission(commission=0.001)
 
 # Print out the starting conditions
 print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
