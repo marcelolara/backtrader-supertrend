c9891fad476a862eec62e7cbee0ebca414f314f6
Marcelo Lara <chelop@gmail.com>
2021-10-24 18:13:45 -0300

    Optimizing the SMA

--- backtest.py
+++ backtest.py
@@ -1,6 +1,7 @@
-import datetime  # For datatime objects
+import datetime  # For datetime objects
 import os.path  # To manage paths
 import sys  # To find out the script name (in argv[0])
+
 
 # Import the backtrader platform
 import backtrader as bt
@@ -10,12 +11,14 @@ import backtrader as bt
 class TestStrategy(bt.Strategy):
     params = (
         ('maperiod', 15),
+        ('printlog', False),
     )
 
-    def log(self, txt, dt=None):
+    def log(self, txt, dt=None, doprint=False):
         """ Logging function for this strategy """
-        dt = dt or self.datas[0].datetime.date(0)
-        print('%s, %s' % (dt.isoformat(), txt))
+        if self.params.printlog or doprint:
+            dt = dt or self.datas[0].datetime.date(0)
+            print('%s, %s' % (dt.isoformat(), txt))
 
     def __init__(self):
         # Keep a reference to the "close" line in the data[0] dataseries
@@ -27,17 +30,8 @@ class TestStrategy(bt.Strategy):
         self.buycomm = None
 
         # Add a MovingAverageSimple indicator
-        self.sma = bt.indicators.SMA(
+        self.sma = bt.indicators.SimpleMovingAverage(
             self.datas[0], period=self.params.maperiod)
-
-        # Indicators for the plotting show
-        bt.indicators.ExponentialMovingAverage(self.datas[0], period=25)
-        bt.indicators.WeightedMovingAverage(self.datas[0], period=25, subplot=True)
-        bt.indicators.StochasticSlow(self.datas[0])
-        bt.indicators.MACDHisto(self.datas[0])
-        rsi = bt.indicators.RSI(self.datas[0])
-        bt.indicators.SmoothedMovingAverage(rsi, period=10)
-        bt.indicators.ATR(self.datas[0], plot=False)
 
     def notify_order(self, order):
         if order.status in [order.Submitted, order.Accepted]:
@@ -90,10 +84,11 @@ class TestStrategy(bt.Strategy):
 
             # Not yet ... we MIGHT BUY if ...
             if self.dataclose[0] > self.sma[0]:
+
                 # BUY, BUY, BUY!!! (with all possible default parameters)
                 self.log('BUY CREATE, %.2f' % self.dataclose[0])
 
-                # Keep track of the created order to avoid an 2nd order
+                # Keep track of the created order to avoid a 2nd order
                 self.order = self.buy()
 
         else:
@@ -102,35 +97,41 @@ class TestStrategy(bt.Strategy):
                 # SELL, SELL, SELL!!! (with all possible default parameters)
                 self.log('SELL CREATE, %.2f' % self.dataclose[0])
 
-                # Keeep track of the created order to avoid a 2nd order
+                # Keep track of the created order to avoid a 2nd order
                 self.order = self.sell()
+
+    def stop(self):
+        self.log('(MA Period %2d) Ending Value %.2f' %
+                 (self.params.maperiod, self.broker.getvalue()), doprint=True)
 
 
 # Create a cerebro entity
 cerebro = bt.Cerebro()
 
-# add a strategy
-cerebro.addstrategy(TestStrategy)
+# Add a strategy
+strats = cerebro.optstrategy(
+    TestStrategy,
+    maperiod=range(10, 31))
 
-# Datas are in a subfolder of samples. Need to find where the script is
+# Datas are in a subfolder of the samples. Need to find where the script is
 # because it could have been called from anywhere
 modpath = os.path.dirname(os.path.abspath(sys.argv[0]))
 datapath = os.path.join(modpath, 'datas/orcl-1995-2014.txt')
 
 # Create a Data Feed
-data = bt.feeds.YahooFinanceData(
+data = bt.feeds.YahooFinanceCSVData(
     dataname=datapath,
     # Do not pass values before this date
     fromdate=datetime.datetime(2000, 1, 1),
     # Do not pass values before this date
     todate=datetime.datetime(2000, 12, 31),
-    # Do not pass valutes after this date
+    # Do not pass values after this date
     reverse=False)
 
 # Add the Data Feed to Cerebro
 cerebro.adddata(data)
 
-# Set oru desired cash start
+# Set our desired cash start
 cerebro.broker.setcash(1000.0)
 
 # Add a FixedSize sizer according to the stake
@@ -139,14 +140,5 @@ cerebro.addsizer(bt.sizers.FixedSize, stake=10)
 # Set the commission - 0.1% ... divide by 100 to remove the %
 cerebro.broker.setcommission(commission=0.0)
 
-# Print out the starting conditions
-print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
-
 # Run over everything
-cerebro.run()
-
-# Print the final result
-print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
-
-# Plot the result
-cerebro.plot()
+cerebro.run(maxcpus=1)
